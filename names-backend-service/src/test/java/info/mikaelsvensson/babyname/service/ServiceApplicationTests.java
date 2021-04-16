b90d8c78b3a43a54227836e613c03ab269c960f8
Mikael Svensson <mikael.svensson@klarna.com>
2021-04-16 11:01:34 +0200

    Fix test #13

--- names-backend-service/src/test/java/info/mikaelsvensson/babyname/service/ServiceApplicationTests.java
+++ names-backend-service/src/test/java/info/mikaelsvensson/babyname/service/ServiceApplicationTests.java
@@ -2,8 +2,10 @@ package info.mikaelsvensson.babyname.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles({"test", "email-smtp", "db"})
 class ServiceApplicationTests {
 
 	@Test
