4fac37d8e3e88901962bd9073f08a141efc0cd6a
Mikael Svensson <mikael.svensson@klarna.com>
2022-07-26 23:46:08 +0200

    Remove unused Spring profile #34

--- names-backend-service/src/test/java/info/mikaelsvensson/babyname/service/controller/AnonymousAuthenticatorControllerTest.java
+++ names-backend-service/src/test/java/info/mikaelsvensson/babyname/service/controller/AnonymousAuthenticatorControllerTest.java
@@ -26,7 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles({"test", "email-smtp", "db"})
+@ActiveProfiles({"test", "email-smtp"})
 class AnonymousAuthenticatorControllerTest {
     @Autowired
     private MockMvc mockMvc;
